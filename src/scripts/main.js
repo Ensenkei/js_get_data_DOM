46fb43d69b6f4326779bf07df7caf60d945d2ebb
Oleksandr Polishchuk <crazy8diamond@gmail.com>
2025-05-28 12:57:50 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+function parsePopulation(text) {
+  return Number(text.replaceAll(',', ''));
+}
+
+const populationElements = document.querySelectorAll('.population');
+
+const populatinNumbers = Array.from(populationElements)
+  .map((el) => parsePopulation(el.textContent))
+  .filter((num) => !isNaN(num));
+
+const total = populatinNumbers.reduce((sum, num) => sum + num, 0);
+const average = total / populatinNumbers.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = total.toLocaleString('en-US');
+averageElement.textContent = average.toLocaleString('en-US');
